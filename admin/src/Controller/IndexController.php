d57cb9d153fb514fe7c176a5f8416d1e3e083314
tamakiii <tamakiii@users.noreply.github.com>
2020-07-16 00:08:44 +0900

    rm GraphQL from IndexController

--- admin/src/Controller/IndexController.php
+++ admin/src/Controller/IndexController.php
@@ -14,40 +14,6 @@ class IndexController
 {
   public function index(Request $request): Response
   {
-    $queryType = new ObjectType([
-      'name' => 'Query',
-      'fields' => [
-        'echo' => [
-          'type' => Type::string(),
-          'args' => [
-            'message' => Type::nonNull(Type::string()),
-          ],
-          'resolve' => function($root, $args) {
-            return $root['prefix'] . $args['message'];
-          }
-        ]
-      ],
-    ]);
-
-    $schema = new Schema([
-      'query' => $queryType,
-    ]);
-
-    $input = json_decode($request->getContent(), true);
-    $query = $input['query'] ?? null;
-    $variableValues = $input['variables'] ?? null;
-
-    try {
-      $rootValue = ['prefix' => 'You said: '];
-      $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
-      $output = $result->toArray();
-      return new JsonResponse($output);
-    } catch (\Exception $e) {
-      $output = [
-        'errors' => [
-          ['message' => $e->getMessage()]
-        ]
-      ];
-    }
+    return new JsonResponse(['message' => 'hi']);
   }
 }
