2d7a2ffe0cb5af364f77c352be78ad97a179958a
tamakiii <tamakiii@users.noreply.github.com>
2020-07-15 23:56:19 +0900

    using dataSources

--- graphql/src/main.js
+++ graphql/src/main.js
@@ -3,12 +3,27 @@ const path = require('path')
 const { ApolloServer, gql } = require('apollo-server')
 const schema = fs.readFileSync(path.join(__dirname, 'schema.graphql'))
 
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class Hello extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://nginx/';
+  }
+
+  async getHello() {
+    const result = this.get(`hello`)
+    console.log(result)
+    return result
+  }
+}
+
 const typeDefs = gql`${schema}`
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return Date.now()
+    hello: async (source, _, { dataSources }) => {
+      return dataSources.hello.getHello();
     }
   }
 }
@@ -16,6 +31,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  dataSources: () => ({
+    hello: new Hello
+  }),
   introspection: true,
   playground: true,
 })
